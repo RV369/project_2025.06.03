c2f826f817c5d6b734af6f1ada46f5062e27cdbf
Yury Demidov <laikr@rambler.ru>
2025-06-04 16:26:28 +0300

    2025/06/04(6)

--- src/core.py
+++ src/core.py
@@ -1,6 +1,6 @@
-from collections import Counter
-
-
 async def count_str(string: str, item: list) -> int:
     """Подсчитывает частоту слов"""
-    return Counter([i.content for i in item])[string]
+    m = 0
+    for post in item:
+        m += post.content.count(string)
+    return m
